57216882afada3a05d45cccbc18f6d82be1b0b44
Rose070903 <roseannpadin4@gmail.com>
2025-06-10 13:00:20 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle("active", i === index);
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+showSlide(currentSlide);
+setInterval(nextSlide, 4000); // Change every 5 seconds
